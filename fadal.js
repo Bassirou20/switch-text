d647f5b06cf5f53e09aeaa43de4343216d1a14d3
Fadal1011 <Mouhamadoufadal.ndiaye@uvs.edu.sn>
2023-03-07 13:08:00 +0000

    suppression d'evenement

--- fadal.js
+++ fadal.js
@@ -2,13 +2,25 @@ let droite = document.querySelector('.droit');
 let btn_droite = document.querySelector('.fa-arrow-right');
 let tabGauche = gauche.childNodes;
 
-function moveRight(){
-    let element = this;
-    element.style.background="red";
-    btn_droite.addEventListener('click',()=>{
-        droite.appendChild(element);
+function remove(){
+    let paras = document.querySelectorAll('.gauche p')
+    paras.forEach(function(para){
+        para.classList.remove('select');
     })
 }
+
+function moveRight(){
+    let element = this;
+    remove();
+   element.classList.add("select");
+}
+
+btn_droite.addEventListener('click',()=>{
+   let element=document.querySelector('.select')
+   element.removeEventListener('mouseover',moveRight);
+    remove();
+    droite.appendChild(element);
+})
 
 tabGauche.forEach(element=>{
     element.addEventListener('mouseover',moveRight);
